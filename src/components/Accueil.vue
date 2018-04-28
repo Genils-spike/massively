8425ad10d773d747d8a649ccea836fccf04d2c7f
salaris <alexandrebeauverger@gmail.com>
2018-04-28 18:37:42 +0200

    initialization of vue

---
+++ src/components/Accueil.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Accueil',
+  components: {
+
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
